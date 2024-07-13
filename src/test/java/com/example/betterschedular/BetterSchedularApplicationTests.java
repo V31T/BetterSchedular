8c2d888c04ecf0cae1f83e0e576d0ebda050ea02
Henry Pham <92609058+V31T@users.noreply.github.com>
2024-07-13 02:43:23 -0700

    first commit

---
+++ src/test/java/com/example/betterschedular/BetterSchedularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.betterschedular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterSchedularApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
